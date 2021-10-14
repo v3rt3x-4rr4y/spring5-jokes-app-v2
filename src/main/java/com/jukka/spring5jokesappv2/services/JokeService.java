dc1fe4ff8e8f4a04d669b52cf0f5ba3158b4e5d4
jukka@pwnlab <jukka@pwnlab.com>
2021-10-14 14:44:25 +0100

    Implemented joke service, closes #2.

---
+++ src/main/java/com/jukka/spring5jokesappv2/services/JokeService.java
@@ -0,0 +1,6 @@
+package com.jukka.spring5jokesappv2.services;
+
+public interface JokeService
+{
+    String getJoke();
+}
---
+++ src/main/java/com/jukka/spring5jokesappv2/services/JokeServiceImpl.java
@@ -0,0 +1,22 @@
+package com.jukka.spring5jokesappv2.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokeServiceImpl implements JokeService
+{
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl()
+    {
+        // NB: not using constructor dependency injection at this time !
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String getJoke()
+    {
+        return this.chuckNorrisQuotes.getRandomQuote();
+    }
+}
